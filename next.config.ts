f686fb32e698b89aeeadecdd72a663367b283525
yigitergun <yigitergunn4@gmail.com>
2025-11-25 20:16:13 +0100

    sa

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/valmore_collective",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
